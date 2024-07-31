01dcff39b0c471053d867385dd3505c187787211
Meher Yar Khan <meheryarkhan90@hotmail.com>
2024-07-31 11:03:53 +0300

    Refactor code to make App Smart and Component Dumb

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,17 +1,12 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, SyntheticEvent } from 'react'
 
-type Props = {}
+interface Props {
+    onClick: (e: SyntheticEvent) => void;
+    search: string | undefined;
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
 
-const Search: React.FC<Props> = (props: Props): JSX.Element => {
-    const [search, setSearch] = useState<string>("");
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-        console.log(e);
-    };
-    const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        console.log(e);
-    };
-
+const Search: React.FC<Props> = ({ onClick, search, handleChange }: Props): JSX.Element => {
 
     return (
         <div>
