9ee524ac49727c49edd556d90f9da00c3fced8d1
Meher Yar Khan <meheryarkhan90@hotmail.com>
2024-08-05 11:30:49 +0300

    React Router Working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,66 +1,14 @@
-import { SyntheticEvent, useState } from 'react';
+import { Outlet } from 'react-router';
 import './App.css';
-import CardList from './Components/CardList/CardList';
-import Search from './Components/Search/Search';
-import { CompanySearch } from './company.d';
-import { searchCompanies } from './api';
-import ListPortfolio from './Components/Portfolio/ListPortfolio/ListPortfolio';
 import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
 
 function App() {
 
-  const [searchQuery, setSearchQuery] = useState<string>("");
-  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
-  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string>("");
-
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value);
-  };
-
-  const onSearchSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    const result = await searchCompanies(searchQuery);
-
-    if (typeof result === "string") {
-      setServerError(result);
-    } else if (Array.isArray(result.data)) {
-      setSearchResult(result.data);
-    }
-
-    console.log(searchResult);
-  };
-
-  const onPortfolioCreate = (e: any) => {
-    e.preventDefault();
-    const exists = portfolioValues.find(value => value === e.target[0].value)
-    if (exists) return;
-    const updatedPortfolio = [...portfolioValues, e.target[0].value];
-    setPortfolioValues(updatedPortfolio);
-    console.log(portfolioValues);
-  }
-
-  const onPortfolioDelete = (e: any) => {
-    e.preventDefault();
-    const removed = portfolioValues.filter((value) => {
-      return value !== e.target[0].value;
-    });
-    setPortfolioValues(removed);
-  }
-
-
   return (
-    <div className="App">
+    <>
       <Navbar />
-      <Search onSearchSubmit={onSearchSubmit}
-        search={searchQuery}
-        handleSearchChange={handleSearchChange} />
-      {serverError && <h1>{serverError}</h1>}
-      <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete} />
-      <CardList searchResults={searchResult}
-        onPortfolioCreate={onPortfolioCreate} />
-    </div>
+      <Outlet />
+    </>
   );
 }
 
---
+++ frontend/src/Pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,63 @@
+import React, { SyntheticEvent, useState } from 'react'
+import Search from '../../Components/Search/Search'
+import ListPortfolio from '../../Components/Portfolio/ListPortfolio/ListPortfolio'
+import CardList from '../../Components/CardList/CardList'
+import { CompanySearch } from '../../company.d'
+import { searchCompanies } from '../../api'
+
+interface Props { }
+
+const SearchPage = (props: Props) => {
+    const [searchQuery, setSearchQuery] = useState<string>("");
+    const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
+    const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+    const [serverError, setServerError] = useState<string>("");
+
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(e.target.value);
+    };
+
+    const onSearchSubmit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+        const result = await searchCompanies(searchQuery);
+
+        if (typeof result === "string") {
+            setServerError(result);
+        } else if (Array.isArray(result.data)) {
+            setSearchResult(result.data);
+        }
+
+        console.log(searchResult);
+    };
+
+    const onPortfolioCreate = (e: any) => {
+        e.preventDefault();
+        const exists = portfolioValues.find(value => value === e.target[0].value)
+        if (exists) return;
+        const updatedPortfolio = [...portfolioValues, e.target[0].value];
+        setPortfolioValues(updatedPortfolio);
+        console.log(portfolioValues);
+    }
+
+    const onPortfolioDelete = (e: any) => {
+        e.preventDefault();
+        const removed = portfolioValues.filter((value) => {
+            return value !== e.target[0].value;
+        });
+        setPortfolioValues(removed);
+    }
+
+    return (
+        <div className="App">
+            <Search onSearchSubmit={onSearchSubmit}
+                search={searchQuery}
+                handleSearchChange={handleSearchChange} />
+            {serverError && <h1>{serverError}</h1>}
+            <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete} />
+            <CardList searchResults={searchResult}
+                onPortfolioCreate={onPortfolioCreate} />
+        </div>
+    )
+}
+
+export default SearchPage
